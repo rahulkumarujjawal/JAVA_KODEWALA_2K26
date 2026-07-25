2a8fa0e4eaf34d9258d88aa1d41c7c03927dec14
RAHUL UJJAWAL <vikashkumarjyoti@VIKASHs-MacBook-Air.local>
2026-07-25 15:35:08 +0530

    Update Product Class

--- DO-CODE-DAILY/src/com/abstracts/interfaces/Product.java
+++ DO-CODE-DAILY/src/com/abstracts/interfaces/Product.java
@@ -1,79 +1,75 @@
 package com.abstracts.interfaces;
 
-public abstract class Product
-{
-	 public String productName;
-	 public String productId;
-	 public int price;
-	 public int productLocation;
-	 
-	 
-	 public void productDetails() {
-		 System.out.println("Product Name :- " + productName);
-	 }
-	 
-	 abstract void productBrand();         // not implementations   abstract class
-	 abstract void productType();
+public abstract class Product {
+	public String productName;
+	public String productId;
+	public int price;
+	public int productLocation;
 
-	 
+	public void productDetails() {
+		System.out.println("Product Name :- " + productName);
+	}
+
+	abstract void productBrand(); // not implementations abstract class
+
+	abstract void productType();
+
 }
 
-
-class Electronic extends Product{
+class Electronic extends Product {
 
 	@Override
 	void productBrand() {
 		System.out.println("Details All Electronic Product");
-		
+
 	}
 
 	@Override
 	void productType() {
 		System.out.println("Details all Data");
-		
+
 	}
-	
+
 }
 
-
-class Cloth extends Product{
+class Cloth extends Product {
 
 	@Override
 	void productBrand() {
 		System.out.println("");
-		
+
 	}
 
 	@Override
 	void productType() {
 		System.out.println();
-		
+
 	}
-	
+
 }
 
-class Payment extends Product{
+class Payment extends Product {
 
 	@Override
 	void productBrand() {
 		System.out.println();
-		
+
 	}
 
 	@Override
 	void productType() {
 		System.out.println();
-		
+
 	}
-	
-class Driver {
-	public static void main(String[] args) {
-		
-		Product product  = new Electronic();
-		product.productDetails();
-		product.productBrand();
-		
+
+	class Driver {
+		public static void main(String[] args) {
+
+			Product product = new Electronic();
+			product.productDetails();
+			product.productBrand();
+
+		}
 	}
-}
-	
+
 }
